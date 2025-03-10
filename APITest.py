73edb1c9c599dfc1e404a8d0d6befd4dde165421
Faizan256 <88046266+Faizan256@users.noreply.github.com>
2025-03-10 13:40:51 -0500

    Add files via upload

---
+++ APITest.py
@@ -0,0 +1,46 @@
+
+#Faizan-Ali Lalani
+#FBL230000
+
+
+from pymongo import MongoClient
+from atproto import Client
+
+
+# Disaster keywords
+disaster_keywords = ["earthquake", "flood", "hurricane", "fire", "tsunami", "tornado", "disaster", "emergency"]
+
+# Connect to MongoDB
+client_mongo = MongoClient("mongodb+srv://Twitterdb:CS4485.0W1@cluster0.toqut.mongodb.net/")
+#mongodb+srv://Twitterdb:CS4485.0W1@cluster0.toqut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+database = client_mongo["bluesky_db"]
+collection = database["disaster_feed"]
+
+# Connect to Bluesky API
+client_bsky = Client()
+
+# Enter Bluesky user and password 
+client_bsky.login('bluesky-login', 'password')
+
+# Get All of Bluesky Posts
+feed = client_bsky.app.bsky.feed.get_timeline()
+
+# Filter out disaster-related posts, based on the text 
+for post in feed['feed']:
+    #record = post.get('record', {}) ??
+    #content_text = record.get('text', '').lower() ??
+    content_text = post['record']['text'].lower()
+
+# Filter for disaster based on keywords
+    if any(keyword in content_text for keyword in disaster_keywords):  
+        data = {
+            "post_id": post['uri'],
+            #"author": post['author']['handle'], (If needed)
+            "location": post.get('location', 'Unknown'),
+            "content": post['record']['text'],
+            #"created_at": post['record']['createdAt'] (If Needed)
+        }
+        # Prevent duplicates
+        collection.update_one({"post_id": data["post_id"]}, {"$set": data}, upsert=True)  
+
+print("Disaster feed updated successfully.")
