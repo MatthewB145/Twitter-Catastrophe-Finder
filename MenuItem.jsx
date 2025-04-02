3d46e8433f897961da83a81248189b44bea47583
Gerardo314 <128551623+Gerardo314@users.noreply.github.com>
2025-04-02 14:14:40 -0500

    updated formatting

--- MenuItem.jsx
+++ MenuItem.jsx
@@ -2,11 +2,8 @@ import "./MenuItem.css"
 
 export default function MenuItem({name,location,report,date,level,class1})
 {
-    
     let class2 = "menuitem";
-    level =="minor"? class2= class2+ " minor":  class2= class2+ " major";
     class2 = class2+" "+class1;
-    console.log("Full classes: "+class2);
     return(
         <div className= {class2}> 
             <h3 >{name}</h3>
