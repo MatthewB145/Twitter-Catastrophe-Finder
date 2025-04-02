f29a74a04d012e8b0209f8655c30970985b6b591
Gerardo314 <128551623+Gerardo314@users.noreply.github.com>
2025-04-02 14:11:23 -0500

    Updated to handle filter changes
    The Interactive map component now handles the changes in the filter checkboxes. It uses a state hook in order to see changes in the checkboxes and updates the array containing the disaster objects based on the disaster types that are currently clicked.

--- InteractiveMap.jsx
+++ InteractiveMap.jsx
@@ -1,30 +1,46 @@
 import Map from "./Map";
 import Menu from "./Menu";
-import Graph from "./Graph";
+import StatCard from "./StatCard";
 import React, { useState } from 'react';
 import "./Interactive-Map.css"
+import Graph from "./Graph";
+import StatDash from "./StatDash";
+import SelectionMenu from "./SelectionMenu";
+import { DISASTERS } from "./data";
+import SignificantEvent from "./SignificantEvent";
 
-export default function InteractiveMap() {
-  
+export default function InteractiveMap({disasterData}) {
+    const [selectedDisasters, setSelectedDisasters] = useState(["Earthquake", "Fire", "Flood", "Hurricane","Tornado"]);
+
+    const handleFilterChange = (disasterType) => {
+        setSelectedDisasters((prev) =>
+        prev.includes(disasterType)
+            ? prev.filter((d) => d !== disasterType)
+            : [...prev, disasterType]
+        );
+        
+    };
+    
+    const filteredData= selectedDisasters.length
+    ? disasterData.filter((disaster) => selectedDisasters.includes(disaster.name))
+    : DISASTERS;
 
     return(
         
         
         <div className='Map-Menu-Container'>
-            
-            <h2 className = "title">Disaster Alerts & Reports</h2>
+            <h1 className="heading">Disaster Alerts & Reports</h1>
+           <StatDash data = {disasterData}></StatDash>
+           <SelectionMenu selectedDisasters={selectedDisasters} onFilterChange={handleFilterChange}></SelectionMenu>
             <div className='Map-Menu-Wrapper'>
-                <Menu/>
-                <div className='Graph-Container'>
-                    <Map/>
-                    <Graph/>
-                    
+                <Menu disasters = {filteredData}/>
+                <div className="Map-Graph-Contain">
+                    <Map Disasters ={filteredData}/>
+                    <SignificantEvent data = {disasterData}></SignificantEvent>
                 </div>
+                
+                
             </div>
-
-            
-
         </div>
-        
     );
 }
