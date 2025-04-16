e0c6e8d78b058a76bec3136ba3b0ad2cd1d86ae9
Gerardo314 <128551623+Gerardo314@users.noreply.github.com>
2025-04-16 01:16:05 -0500

    Update StatCard.jsx capitalize

--- src/StatCard.jsx
+++ src/StatCard.jsx
@@ -2,13 +2,13 @@ import "./StatCard.css"
 
 export default function StatCard({disaster,count})
 {
-    
-    let icon = `${disaster}.png`
+    let disasterName = disaster.charAt(0).toUpperCase() + disaster.slice(1);
+    let icon = `${disasterName}.png`
     return(
         <div className="statcard" > 
             <div><img src = {icon} alt ={icon+ " logo"}></img></div>
             <h3>{count}</h3>
-            <p>{disaster}</p>
+            <p>{disasterName}</p>
         </div>
     );
 }
